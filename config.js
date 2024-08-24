05d50198105feb44c0989ffef4e0d31c8e62c72b
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2024-08-24 18:45:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "මෙතනට SESSION_ID එක දෙන්න",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/385059cf39430775732c3.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🚀 Hi Pancha_One_Bot Is Online Now 🕹\n*🚯 Owner* - Real@Pancha\n\n*🚯 Owner Number* -94701391585\n\n*🚯 Whatsapp group* - https://chat.whatsapp.com/Ct5HiBUrMNr3oIwmxEBgn4\n\n_ප්‍රධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
-SUDO_NB: process.env.SUDO_NB || "94742195461", 
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
-MODE: process.env.MODE || "public", 
+MONGODB: process.env.MONGODB || "mongodb url එක දාන්න",
 };
